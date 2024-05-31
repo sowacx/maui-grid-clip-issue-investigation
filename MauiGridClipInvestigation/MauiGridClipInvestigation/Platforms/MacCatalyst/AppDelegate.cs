e61519923b50610ec81bbaf595aac91cf8d680a2
Michal Witczak <mhiz@demant.com>
2024-05-31 10:20:24 +0200

    Revert "first one"
    This reverts commit d4cf6bca8de10a7398851befb051604909041d4c.

--- MauiGridClipInvestigation/MauiGridClipInvestigation/Platforms/MacCatalyst/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace MauiGridClipInvestigation;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
