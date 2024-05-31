0695125f2693906b0db798cbafc34a4fc94cd56c
Michal Witczak <mhiz@demant.com>
2024-05-31 10:27:00 +0200

    first one with proper gitignore

---
+++ MauiGridClipInvestigation/MauiGridClipInvestigation/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MauiGridClipInvestigation;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new AppShell();
+    }
+}
